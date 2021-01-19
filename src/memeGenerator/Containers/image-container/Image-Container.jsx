7da68b2279b7c67cf9fa7b2219ca710ed05f681e
Stephen Paladines <dragon9man@ufl.edu>
2021-01-19 11:37:36 -0500

    Next.js + TailwindCSS project setup

--- src/memeGenerator/Containers/image-container/Image-Container.jsx
+++ src/memeGenerator/Containers/image-container/Image-Container.jsx
@@ -8,27 +8,27 @@ const ImageContainer = () => {
     // Destruct object w/ properties upperText and lowerText
     // Can also be two seperate variables
     upperText: "",
-    lowerText: ""
+    lowerText: "",
   });
 
   const [imageSrc, setImageSrc] = useState("http://lorempixel.com/400/400/"); // Destructs and creates const imageSrc and function setImageSrc
 
   const generateImage = () => {
     fetch("https://picsum.photos/400") // Gets URL from API and then returns a promise which updates image source
-      .then(response => {
+      .then((response) => {
         setImageSrc(response.url);
       })
-      .catch(e => {
+      .catch((e) => {
         // Catches any potential error
         console.log(`Error has occured: ${e}`);
       });
   };
 
-  const handleChange = event => {
+  const handleChange = (event) => {
     const { name, value } = event.target; // Grabs name and value from the event object passed by OnChange (new text on input)
-    setInputText(prevState => ({
+    setInputText((prevState) => ({
       ...prevState, // Preserves the original keys of the state
-      [name]: value // Updates the corresponding key with the new value
+      [name]: value, // Updates the corresponding key with the new value
     }));
   };
 
@@ -37,28 +37,34 @@ const ImageContainer = () => {
     type: "text",
     onChange: handleChange,
     maxLength: "15",
-    className: "text-input"
+    className: "text-input",
   };
 
   const buttonObj = {
     onClick: generateImage,
     className: "page-button",
-    text: "Generate"
+    text: "Generate",
   };
 
-  const inputArray = [{text: "Upper Text", value: upperText, name: "upperText"}, {text: "Lower Text", value:lowerText, name:"lowerText"}]
+  const inputArray = [
+    { text: "Upper Text", value: upperText, name: "upperText" },
+    { text: "Lower Text", value: lowerText, name: "lowerText" },
+  ];
 
   return (
-    <div className="container">  
+    <div className="container">
       <div className="image">
         <img src={imageSrc} alt="" />
         <span id="upperText-label">{upperText}</span>
         <span id="lowerText-label">{lowerText}</span>
       </div>
       <div className="inputs">
-        {inputArray.map(row => 
-        <Input {...row} {...inputObj}></Input>
-        )}
+        <div>
+          dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddssddddd
+        </div>
+        {inputArray.map((row) => (
+          <Input {...row} {...inputObj}></Input>
+        ))}
       </div>
       <Button {...buttonObj} />
     </div>
