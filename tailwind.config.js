899f883bad4481ccdf1682bc7e2b8c5ed8b513c1
Stephen <stephenpaladines@gmail.com>
2021-01-29 17:12:21 -0500

    Fixed - TailwindCSS overriding standard width size

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,12 @@ module.exports = {
       xl: "1200px",
       "2xl": "1440px",
     },
-    width: {
-      "400px": "400px",
-      "full": "100%",
+    extend: {
+      width: {
+        "400px": "400px",
+        "full": "100%",
+      },
     },
-    extend: {},
   },
   variants: {
     extend: {},
