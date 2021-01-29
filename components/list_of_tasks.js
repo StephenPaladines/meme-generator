82db0c603bc20935768c2a406b58624b41808445
Stephen <stephenpaladines@gmail.com>
2021-01-29 17:21:09 -0500

    Updated task component to include default data, controlled state, and form element

--- components/list_of_tasks.js
+++ components/list_of_tasks.js
@@ -3,22 +3,86 @@ import { useEffect, useState } from "react";
 export default function ListOfTasks() {
   const [tasks, setTasks] = useState([]);
 
+  const tasksPlaceholder = [
+    {
+      done: false,
+      id: 123,
+      task: "shower",
+    },
+    {
+      done: true,
+      id: 456,
+      task: "eat",
+    },
+    {
+      done: false,
+      id: 789,
+      task: "sleep",
+    },
+  ];
+
+  const setCheck = (index) => {
+    const check = tasks[index].done;
+    console.log(index, check);
+    setTasks((previous) => {
+      const updatedTasks = [...previous];
+      updatedTasks[index].done = !check;
+      return updatedTasks;
+    });
+  };
+
+  const submitData = () => {
+    fetch("task", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: { tasks },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
   useEffect(() => {
     fetch("/tasks")
       .then((res) => res.json())
-      .then((data) => setTasks(data.tasks));
+      .then((data) => setTasks(data.tasks))
+      .catch((error) => {
+        setTasks([...tasksPlaceholder])
+        console.error("Error:", error);
+      });
   }, []);
 
   return (
-    <>
-      <ul className="list-disc">
-        {tasks.map(({ done, due_date, id, task, user }) => (
-          <li key={id}>
-            <label>{task}</label>
-            <input type="checkbox" checked={!done} onChange={!done}></input>
-          </li>
-        ))}
-      </ul>
-    </>
+    <div className="w-100% m-10">
+      <h1 className="mb-10 text-center bg-gray-400 border border-b-2 heading-1">
+        Tasks
+      </h1>
+      <form className="bg-gray-200" onSubmit={submitData}>
+        <ul className="block w-40 m-auto mx-auto list-none">
+          {tasks.map(({ due_date, id, task, user }, index) => (
+            <li key={id}>
+              <input
+                type="checkbox"
+                onChange={() => setCheck(index)}
+                checked={tasks[index].done}
+              ></input>
+              <label
+                className={`ml-4 ${
+                  tasks[index].done === true ? `line-through` : ``
+                }`}
+              >
+                {task}
+              </label>
+            </li>
+          ))}
+        </ul>
+      </form>
+    </div>
   );
 }
